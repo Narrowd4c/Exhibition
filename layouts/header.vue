1b9ae210b25a73965643f18b1b9ece74f56ca6df
john <aarontu803@gmail.com>
2024-01-17 15:28:03 +0800

    feat: colorMode icon

--- layouts/header.vue
+++ layouts/header.vue
@@ -1,32 +1,43 @@
 <template>
     <div class="container mx-auto p-10">
         <div class="flex">
-            <h1 class="text-4xl tracking-widest font-black hover:text-emerald-400"><RouterLink to="/">MusicBox</RouterLink></h1>
-            <ul class="ms-auto flex gap-4">
+            <h1
+                class="text-4xl font-black tracking-widest hover:text-emerald-400"
+            >
+                <RouterLink to="/">MusicBox</RouterLink>
+            </h1>
+            <ul class="ms-auto flex items-center gap-4">
                 <li title="清單模式">
                     <RouterLink to="/"
-                        ><ListIcon class="dark:fill-white hover:fill-emerald-400"
+                        ><ListIcon
+                            class="hover:fill-emerald-400 dark:fill-white"
                     /></RouterLink>
                 </li>
                 <li title="圖片模式">
                     <RouterLink to="/book"
-                        ><BookIcon class="dark:fill-white hover:fill-emerald-400"
+                        ><BookIcon
+                            class="hover:fill-emerald-400 dark:fill-white"
                     /></RouterLink>
                 </li>
+                <li title="切換主題色" class="h-6">
+                    <button
+                        @click="colorMode.preference = 'light'"
+                        v-if="colorMode.preference === 'dark'"
+                    >
+                        <SunnyIcon class="fill-white hover:fill-yellow-600" />
+                    </button>
+                    <button @click="colorMode.preference = 'dark'" v-else>
+                        <SunnyIcon class="fill-black hover:fill-yellow-600" />
+                    </button>
+                </li>
             </ul>
-            <select
-                class="ms-4 border-2 px-4 py-0 tracking-tighter dark:bg-black"
-                v-model="colorMode.preference"
-            >
-                <option value="light">Light</option>
-                <option value="dark">Dark</option>
-            </select>
         </div>
         <slot></slot>
     </div>
 </template>
 <script setup lang="ts">
 import BookIcon from '../components/icon/MenuBookIcon.vue'
+import SunnyIcon from '../components/icon/sunnyIcon.vue'
 import ListIcon from '../components/icon/ListIcon.vue'
 
 const colorMode = useColorMode()
